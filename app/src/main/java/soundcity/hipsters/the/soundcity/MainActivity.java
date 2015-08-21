4dfbe65be844dd171feff00150a652a449e47236
andrewsilaghi <silaghiandrew89@gmail.com>
2015-08-21 12:16:33 +0300

    commit with list name and location+picture

--- app/src/main/java/soundcity/hipsters/the/soundcity/MainActivity.java
+++ app/src/main/java/soundcity/hipsters/the/soundcity/MainActivity.java
@@ -16,35 +16,24 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        ArrayList<Sound> arrayOfUsers = new ArrayList<Sound>();
-// Create the adapter to convert the array to views
-        UsersAdapter adapter = new UsersAdapter(this, arrayOfUsers);
-// Attach the adapter to a ListView
-        ListView listView = (ListView) findViewById(R.id.listView);
-        listView.setAdapter(adapter);
-
-    }
 
     public class Sound {
         public ImageView img;
         public String name;
         public String location;
 
-        public Sound(String name, String location, ImageView img) {
+        public Sound(ImageView img, String name, String location) {
+            this.img = img;
             this.name = name;
             this.location = location;
-            this.img = img;
+
         }
     }
 
@@ -63,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
             }
             // Lookup view for data population
             TextView tvName = (TextView) convertView.findViewById(R.id.tvName);
-            TextView tvLocation = (TextView) convertView.findViewById(R.id.tvName);
+            TextView tvLocation = (TextView) convertView.findViewById(R.id.tvLocation);
             // Populate the data into the template view using the data object
             tvName.setText(user.name);
             tvLocation.setText(user.location);
@@ -73,7 +62,61 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        ArrayList<Sound> arrayOfUsers = new ArrayList<Sound>();
+// Create the adapter to convert the array to views
+        UsersAdapter adapter = new UsersAdapter(this, arrayOfUsers);
+// Attach the adapter to a ListView
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+//    public class Sound {
+//        // Constructor to convert JSON object into a Java class instance
+//        public Sound(JSONObject object){
+//            try {
+//                this.name = object.getString("name");
+//                this.hometown = object.getString("hometown");
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//        // Factory method to convert an array of JSON objects into a list of objects
+//        // User.fromJson(jsonArray);
+//        public static ArrayList<User> fromJson(JSONArray jsonObjects) {
+//            ArrayList<User> users = new ArrayList<User>();
+//            for (int i = 0; i < jsonObjects.length(); i++) {
+//                try {
+//                    users.add(new User(jsonObjects.getJSONObject(i)));
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//            return users;
+//        }
+//    }
+
+        // Add item to adapter
+        ImageView image = (ImageView) findViewById(R.id.tvImg);
+        //image.setImageResource(R.drawable.ic_launcher_web);
+        Sound newUser = new Sound(image, "Nathan", "San Diego");
+        Sound newUser2 = new Sound(image, "Brian", "Los Angeles");
+        adapter.add(newUser);
+        adapter.add(newUser2);
+        // Or even append an entire new collection
+// Fetching some data, data has now returned
+// If data was JSON, convert to ArrayList of User objects.
+//        JSONArray jsonArray = ...;
+//        ArrayList<Sound> newUsers = Sound.fromJson(jsonArray)
+//        adapter.addAll(newUsers);
+
+
+
+    }
 
 
     @Override
